6d22abbdcc4666fffa0390d83941b961500b0464
19APM <163758230+19APM@users.noreply.github.com>
2024-03-18 13:28:48 -0600

    Add files via upload

---
+++ PR03/PR03.m
@@ -0,0 +1,24 @@
+%% Práctica 3: Señales en tiempo continuo
+%
+%
+%% Integrantes
+%
+% Portillo Martínez Arturo
+%
+%% Objetivos
+%
+% * Manipulación básica de MATLAB
+% * Gráficas de señales reales y complejas continuas
+% * Transformación de señales continuas (escalamientos y traslaciones)
+% * Clculo de energía y potencia de señales continuas
+%
+%% Introducción
+%
+% Se realizó la investigación del uso de Python para la realización de
+% gráficas, a continuación se muestran las gráficas correspondientes al problema 4 mediante Python.
+% 
+% <<C:\Users\porti\OneDrive\Escritorio\Señales y Sistemas\PR03\html\intro.png>>
+%
+% Se puede consultar el código realizado con Python en el siguiente <https://colab.research.google.com/drive/1XnKYDQmnbECtDmMwu0-lw2WxHoELL3KE?usp=sharing enlace> 
+
+
